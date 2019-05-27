ca71cb30d5d0725760df144db0fd3418aba7c86c
Levan Lotuashvili <gwsmaster@gmail.com>
2019-05-28 01:37:20 +0400

    Add TbcLog model and move to Models namespace

---
+++ src/Models/TbcLog.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Lotuashvili\LaravelTbcPay\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TbcLog extends Model
+{
+    /**
+     * @var array Fillable Attributes
+     */
+    protected $fillable = [
+        'transaction_id',
+        'message',
+        'payload',
+    ];
+
+    /**
+     * @var array Attribute Casting
+     */
+    protected $casts = [
+        'payload' => 'array',
+    ];
+
+    /**
+     * TbcLog constructor
+     * Set table name from config
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->table = config('tbc.logs_table', 'tbc_logs');
+    }
+
+    /**
+     * Load transaction relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function transaction()
+    {
+        return $this->belongsTo(TbcTransaction::class);
+    }
+}
